d284f911736f938ad4c7e5183fa849b4e9f0007b
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-02 18:24:09 -0300

    feat: route created to update recipient

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -36,6 +36,35 @@ class RecipientController {
       zip_code,
     });
   }
+
+  async update(req, res) {
+    const { id, email, zip_code } = req.body;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient)
+      return res.status(400).json({ error: 'Recipient not found' });
+
+    if (email && email !== recipient.email) {
+      const recipientExist = await Recipient.findOne({ where: { email } });
+
+      if (recipientExist)
+        return res.status(400).json({ error: 'Recipient already exist' });
+    }
+
+    if (zip_code && zip_code !== recipient.zip_code) {
+      const match = zip_code.match(/\d{5}-\d{3}/g);
+
+      if (!(match && match[0] === zip_code))
+        return res.status(400).json({ error: 'Badly formatted zip code' });
+    }
+
+    const recipientUpdated = await recipient.update(req.body);
+
+    return res.json({
+      recipientUpdated,
+    });
+  }
 }
 
 export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import RecipientController from './app/controllers/RecipientController';
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+
 routes.post('/recipient', RecipientController.store);
+routes.put('/recipient', RecipientController.update);
 
 export default routes;
