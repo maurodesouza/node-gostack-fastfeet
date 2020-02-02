a942f29cf5a6aa59a7e42192709ef45211eb400f
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-02 18:40:22 -0300

    feat: auth middleware added

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization)
+    return res.status(401).json({ error: 'Token not provide' });
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    await promisify(jwt.verify)(token, authConfig.secret);
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import authMiddlware from './app/middleware/auth';
+
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddlware);
 
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient', RecipientController.update);
