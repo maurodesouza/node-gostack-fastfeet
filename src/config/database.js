f709e921ff9abf02c86833749b4f9aec5d879382
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-01 23:22:25 -0300

    build: config sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'apenasumshow',
+  database: 'fastfeed-bootcamp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
