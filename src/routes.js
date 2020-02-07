ddc93287bdb10d4bbab817b3bf9496f853dc4a34
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-07 18:54:43 -0300

    feat: add route to send files

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import authMiddlware from './app/middleware/auth';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -14,9 +15,7 @@ routes.post('/session', SessionController.store);
 
 routes.use(authMiddlware);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient', RecipientController.update);
